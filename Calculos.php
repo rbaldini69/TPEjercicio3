6c50ba185be4d9541518545ceb8374ddf2755b48
rbaldini69 <rbaldini69@hotmail.com>
2016-04-09 23:27:37 -0300

    Update and rename Index.php to Calculos.php

--- Calculos.php
+++ Calculos.php
@@ -3,12 +3,9 @@
 	$valorIngresado=0;
 	if(isset($_GET["afahrenheit"]))
 	{
-
 		$valorIngresado=$_GET["temperatura"];
 		$afahrenheit=($valorIngresado*9/5)+32;
 	$muestro=$valorIngresado." grados Centigrados equivalen a :".$afahrenheit." grados Fahreinheit.-";
-
-
 	}
 	if (isset($_GET["acentigrados"])) 
 	{
@@ -16,11 +13,5 @@
 		$acentigrados=(($valorIngresado-32)*5)/9;
 		$muestro=$valorIngresado." grados Fahreinheit equivalen a :".$acentigrados." Centigrados.-";
 	}
-
-		
 			include "index.php";
-		
-
-	
-	
 ?>
