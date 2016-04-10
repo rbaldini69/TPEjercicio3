a43e7c441c0b6265ab9fe1d834b68cd024a7c99f
rbaldini69 <rbaldini69@hotmail.com>
2016-04-09 23:25:48 -0300

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,26 @@
+<?php
+	
+	$valorIngresado=0;
+	if(isset($_GET["afahrenheit"]))
+	{
+
+		$valorIngresado=$_GET["temperatura"];
+		$afahrenheit=($valorIngresado*9/5)+32;
+	$muestro=$valorIngresado." grados Centigrados equivalen a :".$afahrenheit." grados Fahreinheit.-";
+
+
+	}
+	if (isset($_GET["acentigrados"])) 
+	{
+		$valorIngresado=$GET["temperatura"];
+		$acentigrados=(($valorIngresado-32)*5)/9;
+		$muestro=$valorIngresado." grados Fahreinheit equivalen a :".$acentigrados." Centigrados.-";
+	}
+
+		
+			include "index.php";
+		
+
+	
+	
+?>
