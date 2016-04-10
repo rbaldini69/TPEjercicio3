f2651546292b2ffb581480f9bf93a4c10513ce84
rbaldini69 <rbaldini69@hotmail.com>
2016-04-09 23:28:32 -0300

    Update and rename Calculo.php to Index.php

--- Index.php
+++ Index.php
@@ -2,8 +2,6 @@
 <html>
 <head>
 	<title></title>
-	
-
 <h1 style="text-align: center; font-size: 15;text-decoration-color: #B40431"> Conversor de temperatura </h1>
 <body style="background-color: lightblue">
 <div style="border-style: solid;border-color: orange;margin-left: 28%; margin-right: 40%">
@@ -14,17 +12,10 @@
 	
 	<br> <input style="font-size: 16px;height: 40px;width: 200px;margin-left: 18%; border-radius:5px" type="double" value="<?php echo $valoringresado; ?>" name="temperatura"></br>
 	<p><br></p>
-	
-	
 	</div>
-
 <div style="">
 <br><input style="height: 40px;width: 200px; border-radius:5px; margin-left: 18%;margin-right: 30%" type="submit" name="acentigrados" value="A - De fahrenheit a centigrados"> </br>
-
-
 <br><input style="height: 40px;width: 200px; border-radius:5px;margin-left: 18%" type="submit" name="afahrenheit" value="A - De centigrados a fahrenheit"> </br>
-
-
 	<div style="height: 40px; text-align: center; border-style: solid; border-color: blue; margin-left: 0.3%">
 		<?php
 		echo $muestro;
